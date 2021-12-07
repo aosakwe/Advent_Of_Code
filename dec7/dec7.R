064e8d82271494f4cc0db956742c017dc973ab85
Adrien Osakwe <75806865+aosakwe@users.noreply.github.com>
2021-12-07 10:41:36 -0500

    Update dec7.R

--- dec7/dec7.R
+++ dec7/dec7.R
@@ -2,13 +2,15 @@
 
 input <- as.numeric(read.table(file = 'test.txt',sep = ',',))
 val <- data.frame(input)
-pos <- unique(input)
 # Part 1 #
 
+#Storing the position with the least distance/fuel covered/used for all crabs to reach the point
 shortest_dist <- 99999999999
 position <- 9999999999
-for (num in 1:max(pos)){
+for (num in 1:max(val$input)){
+  #store distance/fuel for all crabs to reach the position
   val$shift <- abs(num - val$input)
+  #if the fuel/distance is smaller than current smallest value, update
   if (sum(val$shift) <= shortest_dist){
     shortest_dist <- sum(val$shift)
     position <- num
@@ -20,7 +22,9 @@ shortest_dist
 # Part 2 #
 shortest_dist <- 99999999999
 position <- 9999999999
-for (num in 1:max(pos)){
+for (num in 1:max(val$input)){
+  #same solution as above except shift now stores to sum of a finite arithmetic series with a difference of 1
+  #since each additional shift of 1 uses one more fuel than the previous shift, the fuel used follows an arithmetic sequence
   val$shift <- abs(num - val$input)*(1+abs(num - val$input))/2
   if (sum(val$shift) <= shortest_dist){
     shortest_dist <- sum(val$shift)
