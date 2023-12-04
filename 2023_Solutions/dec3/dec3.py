075dbf0605e7b2c050d5ca7d78111e6d3f23b8ba
Adrien Osakwe <adrien.osakwe@mail.mcgill.ca>
2023-12-03 21:21:05 -0500

    finished day 3

--- 2023_Solutions/dec3/dec3.py
+++ 2023_Solutions/dec3/dec3.py
@@ -37,19 +37,13 @@ def search_grid(grid):
                 else:
                     cur_num += grid[row][col]
                 if not is_part:
-                    is_part = check_neighbors(row,col,grid,len(grid[row]),len(grid))
-                if col == len(grid[row])-1:
-                    if is_part:
-                       #print(cur_num)
-                        part_sum += int(cur_num)
-                    is_part,no_num,cur_num = reset_search()
+                    is_part = check_neighbors(row,col,grid,len(grid[row]),len(grid))              
             elif is_part:
                 #print(cur_num)
                 part_sum += int(cur_num)
                 is_part,no_num,cur_num = reset_search()
             else:
                 is_part,no_num,cur_num = reset_search()
-        is_part,no_num,cur_num = reset_search()
     return part_sum
 
 grid = build_grid(input)
@@ -61,18 +55,18 @@ print(search_grid(grid))
 #Part 2
 #returns list of coord tuples
 def check_gears(x,y,grid,grid_col,grid_row):
-    is_gear = []
+    is_gear = None
     x_coords = [x-1,x,x+1]
     y_coords = [y-1,y,y+1]
     for xc in x_coords:
         for yc in y_coords:
             if xc >= 0 and xc < grid_row and yc >=0 and yc < grid_col:
+               
                 if grid[xc][yc] == '*':
-                    is_gear.append((xc,yc))
-    if len(is_gear) > 1:
-        return is_gear
-    else:
-        return None
+                    
+                    is_gear = (xc,yc)
+    
+    return is_gear
 
 
 def find_gears(grid):
@@ -89,5 +83,27 @@ def find_gears(grid):
                     cur_num += grid[row][col]
                 if not is_gear:
                     gear_coords = check_gears(row,col,grid,len(grid[row]),len(grid))
+                    if gear_coords != None:
+                        is_gear = True
+            
+            elif cur_num != None:
+                if gear_coords != None:
+                    if gear_coords not in gears.keys():
+                        gears[gear_coords] = [cur_num]
+                    else:
+                        gears[gear_coords].append(cur_num)
+                is_gear,no_num,cur_num = reset_search()
+                gear_coords = None
+            else:
+                is_gear,no_num,cur_num = reset_search()
+                gear_coords = None      
+     
+    #Calculate ratios
+    for coords in gears.keys():
+        if len(gears[coords]) > 1:
+            gear_sum += int(gears[coords][0])*int(gears[coords][1])          
+                    
     
     return gear_sum
+
+print(find_gears(grid))
